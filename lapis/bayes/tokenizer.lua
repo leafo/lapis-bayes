9d64d42fcd6a33c88d03909e5c543a40243d3bf5
leaf corcoran <leafot@gmail.com>
2015-10-09 12:21:51 -0700

    turn tokenizer into class

--- lapis/bayes/tokenizer.lua
+++ lapis/bayes/tokenizer.lua
@@ -1,80 +1,109 @@
 local db = require("lapis.db")
-local filter_tokens
-filter_tokens = function(tokens, opts)
-  local min_len = opts and opts.min_token_length or 2
-  local max_len = opts and opts.max_token_length or 12
-  local strip_numbers = opts and opts.strip_number_tokens or nil
-  if strip_numbers == nil then
-    strip_numbers = true
-  end
-  return (function()
-    local _accum_0 = { }
-    local _len_0 = 1
-    for _index_0 = 1, #tokens do
-      local _continue_0 = false
-      repeat
-        local t = tokens[_index_0]
-        local t_len = #t
-        if t_len > max_len then
-          _continue_0 = true
-          break
-        end
-        if t_len < min_len then
-          _continue_0 = true
-          break
-        end
-        if strip_numbers and t:match("^[%d%.]+$") then
-          _continue_0 = true
-          break
-        end
-        local _value_0 = t
-        _accum_0[_len_0] = _value_0
-        _len_0 = _len_0 + 1
-        _continue_0 = true
-      until true
-      if not _continue_0 then
-        break
+local DefaultTokenizer
+do
+  local _base_0 = {
+    filter_tokens = function(self, tokens)
+      local opts = self.opts
+      local min_len = opts and opts.min_token_length or 2
+      local max_len = opts and opts.max_token_length or 12
+      local strip_numbers = opts and opts.strip_number_tokens or nil
+      if strip_numbers == nil then
+        strip_numbers = true
       end
+      return (function()
+        local _accum_0 = { }
+        local _len_0 = 1
+        for _index_0 = 1, #tokens do
+          local _continue_0 = false
+          repeat
+            local t = tokens[_index_0]
+            local t_len = #t
+            if t_len > max_len then
+              _continue_0 = true
+              break
+            end
+            if t_len < min_len then
+              _continue_0 = true
+              break
+            end
+            if strip_numbers and t:match("^[%d%.]+$") then
+              _continue_0 = true
+              break
+            end
+            local _value_0 = t
+            _accum_0[_len_0] = _value_0
+            _len_0 = _len_0 + 1
+            _continue_0 = true
+          until true
+          if not _continue_0 then
+            break
+          end
+        end
+        return _accum_0
+      end)()
+    end,
+    tokenize_text = function(self, text)
+      local opts = self.opts
+      local pre_filter = opts and opts.filter_text
+      if pre_filter then
+        text = pre_filter(text)
+      end
+      if opts and opts.strip_tags then
+        local extract_text
+        extract_text = require("web_sanitize").extract_text
+        text = extract_text(text)
+      end
+      if opts and opts.symbols_split_tokens then
+        text = text:gsub("[%!%@%#%$%%%^%&%*%(%)%[%]%{%}%|%\\%/%`%~%-%_%<%>%,%.]", " ")
+      end
+      local res = db.query([[      select unnest(lexemes) as word
+      from ts_debug('english', ?);
+    ]], text)
+      local tokens
+      do
+        local _accum_0 = { }
+        local _len_0 = 1
+        for _index_0 = 1, #res do
+          local r = res[_index_0]
+          _accum_0[_len_0] = r.word
+          _len_0 = _len_0 + 1
+        end
+        tokens = _accum_0
+      end
+      if opts and opts.filter_tokens then
+        tokens = opts.filter_tokens(tokens, opts)
+      else
+        tokens = self:filter_tokens(tokens)
+      end
+      return tokens
     end
-    return _accum_0
-  end)()
+  }
+  _base_0.__index = _base_0
+  local _class_0 = setmetatable({
+    __init = function(self, opts)
+      self.opts = opts
+    end,
+    __base = _base_0,
+    __name = "DefaultTokenizer"
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  DefaultTokenizer = _class_0
 end
 local tokenize_text
 tokenize_text = function(text, opts)
   if opts and opts.tokenize_text then
     return opts.tokenize_text(text, opts)
   end
-  local pre_filter = opts and opts.filter_text
-  if pre_filter then
-    text = pre_filter(text)
-  end
-  if opts and opts.strip_tags then
-    local extract_text
-    extract_text = require("web_sanitize").extract_text
-    text = extract_text(text)
-  end
-  if opts and opts.symbols_split_tokens then
-    text = text:gsub("[%!%@%#%$%%%^%&%*%(%)%[%]%{%}%|%\\%/%`%~%-%_%<%>%,%.]", " ")
-  end
-  local res = db.query([[    select unnest(lexemes) as word
-    from ts_debug('english', ?);
-  ]], text)
-  local tokens
-  do
-    local _accum_0 = { }
-    local _len_0 = 1
-    for _index_0 = 1, #res do
-      local r = res[_index_0]
-      _accum_0[_len_0] = r.word
-      _len_0 = _len_0 + 1
-    end
-    tokens = _accum_0
-  end
-  local filter = opts and opts.filter_tokens or filter_tokens
-  tokens = filter(tokens, opts)
-  return tokens
+  return DefaultTokenizer(opts):tokenize_text(text)
 end
 return {
   tokenize_text = tokenize_text,
-  filter_tokens = filter_tokens
+  DefaultTokenizer = DefaultTokenizer
 }
--- lapis/bayes/tokenizer.moon
+++ lapis/bayes/tokenizer.moon
@@ -1,59 +1,70 @@
 db = require "lapis.db"
 
-filter_tokens = (tokens, opts) ->
-  min_len = opts and opts.min_token_length or 2
-  max_len = opts and opts.max_token_length or 12
-
-  strip_numbers = opts and opts.strip_number_tokens or nil
-  strip_numbers = true if strip_numbers == nil
-
-  return for t in *tokens
-    t_len = #t
-    continue if t_len > max_len
-    continue if t_len < min_len
-
-    if strip_numbers and t\match "^[%d%.]+$"
-      continue
-
-    t
-
--- {
---   tokenize_text: function -- custom tokeniaze function
--- 
---   -- the rest of the options only apply to the default tokenizer
+-- postgres based tokenizer
+-- opts = {
 --   filter_text: function -- function to pre-filter text, returns new text
 --   strip_tags: bool -- remove html tags from input in default
 --   symbols_split_tokens: bool -- symbols split apart tokens
 --   min_len: number -- min length of token (default 2)
 --   max_len: number -- max length of token (default 12)
 --   strip_numbers: bool -- remove tokens that are a number (including decimal)
+-- }
+class DefaultTokenizer
+  new: (@opts) =>
+
+  filter_tokens: (tokens) =>
+    opts = @opts
+    min_len = opts and opts.min_token_length or 2
+    max_len = opts and opts.max_token_length or 12
+
+    strip_numbers = opts and opts.strip_number_tokens or nil
+    strip_numbers = true if strip_numbers == nil
+
+    return for t in *tokens
+      t_len = #t
+      continue if t_len > max_len
+      continue if t_len < min_len
+
+      if strip_numbers and t\match "^[%d%.]+$"
+        continue
+
+      t
+
+  tokenize_text: (text) =>
+    opts = @opts
+    pre_filter = opts and opts.filter_text
+
+    if pre_filter
+      text = pre_filter text
+
+    if opts and opts.strip_tags
+      import extract_text from require "web_sanitize"
+      text = extract_text text
+
+    if opts and opts.symbols_split_tokens
+      text = text\gsub "[%!%@%#%$%%%^%&%*%(%)%[%]%{%}%|%\\%/%`%~%-%_%<%>%,%.]", " "
+
+    res = db.query [[
+      select unnest(lexemes) as word
+      from ts_debug('english', ?);
+    ]], text
+
+    tokens = [r.word for r in *res]
+    tokens = if opts and opts.filter_tokens
+      opts.filter_tokens tokens, opts
+    else
+      @filter_tokens tokens
+
+    tokens
+
+-- {
+--   tokenize_text: function -- custom tokeniaze function
 -- 
 -- }
 tokenize_text = (text, opts) ->
   if opts and opts.tokenize_text
     return opts.tokenize_text text, opts
 
-  pre_filter = opts and opts.filter_text
+  DefaultTokenizer(opts)\tokenize_text text
 
-  if pre_filter
-    text = pre_filter text
-
-  if opts and opts.strip_tags
-    import extract_text from require "web_sanitize"
-    text = extract_text text
-
-  if opts and opts.symbols_split_tokens
-    text = text\gsub "[%!%@%#%$%%%^%&%*%(%)%[%]%{%}%|%\\%/%`%~%-%_%<%>%,%.]", " "
-
-  res = db.query [[
-    select unnest(lexemes) as word
-    from ts_debug('english', ?);
-  ]], text
-
-  tokens = [r.word for r in *res]
-  filter = opts and opts.filter_tokens or filter_tokens
-  tokens = filter tokens, opts
-  tokens
-
-
-{ :tokenize_text, :filter_tokens }
+{ :tokenize_text, :DefaultTokenizer }
