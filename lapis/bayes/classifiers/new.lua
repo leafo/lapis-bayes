2c7f67863be60e775b169c0fc0543cc081515bf0
leaf corcoran <leafot@gmail.com>
2015-10-15 22:59:23 -0700

    make new classifier

--- lapis/bayes/classifiers/new.lua
+++ lapis/bayes/classifiers/new.lua
@@ -1,4 +1,35 @@
 local DefaultClassifier = require("lapis.bayes.classifiers.default")
+local average
+average = function(nums)
+  local sum = 0
+  for _index_0 = 1, #nums do
+    local n = nums[_index_0]
+    sum = sum + n
+  end
+  return sum / #nums
+end
+local weighted_avg
+weighted_avg = function(tuples)
+  local num_tuples = #tuples
+  local sum = 0
+  local sum_weight = 0
+  for _index_0 = 1, #tuples do
+    local _des_0 = tuples[_index_0]
+    local num, weight
+    num, weight = _des_0[1], _des_0[2]
+    sum = sum + num
+    sum_weight = sum_weight + weight
+  end
+  local avg_weight = sum_weight / num_tuples
+  local avg = 0
+  for _index_0 = 1, #tuples do
+    local _des_0 = tuples[_index_0]
+    local num, weight
+    num, weight = _des_0[1], _des_0[2]
+    avg = avg + ((num / num_tuples) * (weight / avg_weight))
+  end
+  return avg
+end
 local NewClassifier
 do
   local _parent_0 = DefaultClassifier
@@ -9,7 +40,6 @@ do
       if not (categories) then
         return nil, available_words
       end
-      local default_prob = self.opts.default_prob or 0.1
       local total_counts = { }
       for _index_0 = 1, #categories do
         local c = categories[_index_0]
@@ -18,35 +48,48 @@ do
           total_counts[word] = total_counts[word] + count
         end
       end
-      require("moon").p(total_counts)
-      for _index_0 = 1, #categories do
-        local c = categories[_index_0]
-        local tuples
-        do
-          local _accum_0 = { }
-          local _len_0 = 1
-          for _index_1 = 1, #available_words do
-            local word = available_words[_index_1]
-            local total_count = total_counts[word]
-            local cat_count = c.word_counts[word] or 0
-            local _value_0 = {
-              word,
-              cat_count / total_count
-            }
-            _accum_0[_len_0] = _value_0
-            _len_0 = _len_0 + 1
+      local probs
+      do
+        local _accum_0 = { }
+        local _len_0 = 1
+        for _index_0 = 1, #categories do
+          local c = categories[_index_0]
+          local tuples
+          do
+            local _accum_1 = { }
+            local _len_1 = 1
+            for _index_1 = 1, #available_words do
+              local word = available_words[_index_1]
+              local total_count = total_counts[word]
+              local cat_count = c.word_counts[word] or 0
+              local _value_0 = {
+                cat_count / total_count,
+                total_count
+              }
+              _accum_1[_len_1] = _value_0
+              _len_1 = _len_1 + 1
+            end
+            tuples = _accum_1
           end
-          tuples = _accum_0
+          local _value_0 = {
+            c.name,
+            weighted_avg(tuples)
+          }
+          _accum_0[_len_0] = _value_0
+          _len_0 = _len_0 + 1
         end
-        print(tostring(c.name))
-        table.sort(tuples, function(a, b)
-          return a[2] > b[2]
-        end)
-        local columnize
-        columnize = require("lapis.cmd.util").columnize
-        print(columnize(tuples))
-        local _ = print
+        probs = _accum_0
       end
+      table.sort(probs, function(a, b)
+        return a[2] > b[2]
+      end)
+      for _index_0 = 1, #probs do
+        local _des_0 = probs[_index_0]
+        local c, p
+        c, p = _des_0[1], _des_0[2]
+        probs[c] = p
+      end
+      return probs, #available_words / #words
     end
   }
   _base_0.__index = _base_0
--- lapis/bayes/classifiers/new.moon
+++ lapis/bayes/classifiers/new.moon
@@ -1,5 +1,29 @@
 DefaultClassifier = require "lapis.bayes.classifiers.default"
 
+average = (nums) ->
+  sum = 0
+  for n in *nums
+    sum += n
+
+  return sum / #nums
+
+weighted_avg = (tuples) ->
+  num_tuples = #tuples
+  sum = 0
+  sum_weight = 0
+
+  for {num, weight} in *tuples
+    sum += num
+    sum_weight += weight
+
+  avg_weight = sum_weight/num_tuples
+
+  avg = 0
+  for {num, weight} in *tuples
+    avg += (num/num_tuples) * (weight/avg_weight)
+
+  avg
+
 class NewClassifier extends DefaultClassifier
   text_probabilities: (categories, text) =>
     categories, available_words, words = @count_words categories, text
@@ -7,37 +31,27 @@ class NewClassifier extends DefaultClassifier
     unless categories
       return nil, available_words
 
-    default_prob = @opts.default_prob or 0.1
-
     total_counts = {}
     for c in *categories
       for word, count in pairs c.word_counts
         total_counts[word] or= 0
         total_counts[word] += count
 
-    require("moon").p total_counts
-
-    for c in *categories
+    probs = for c in *categories
       tuples = for word in *available_words
         total_count = total_counts[word]
         cat_count = c.word_counts[word] or 0
-        {word, cat_count/total_count}
+        {cat_count/total_count, total_count}
 
-      print "#{c.name}"
-      table.sort tuples, (a,b) ->
-        a[2] > b[2]
+      {c.name, weighted_avg tuples}
 
-      import columnize from require "lapis.cmd.util"
-      print columnize tuples
+    table.sort probs, (a,b) ->
+      a[2] > b[2]
 
-      print
+    -- also make probs available in hash
+    for {c, p} in *probs
+      probs[c] = p
 
-      -- by_importance = for t in *tuples
-      --   {math.abs t[2] - 0.5, t}
+    probs, #available_words / #words
 
-      -- table.sort by_importance, (a,b) ->
-      --   a[1] > b[1]
 
-      -- tuples = [i[2] for i in *by_importance]
-      -- require("moon").p tuples
-      -- error "not yet"
