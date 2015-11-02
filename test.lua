5e21358c50ba80fa611aa7add103fb24b13d1c06
leaf corcoran <leafot@gmail.com>
2015-11-01 23:34:01 -0800

    remove test files

--- test.lua
+++
@@ -1,5 +0,0 @@
-local UrlDomainsTokenizer = require("lapis.bayes.tokenizers.url_domains")
-local grammar = UrlDomainsTokenizer():build_grammer()
-return require("moon").p({
-  grammar:match("href='http://leafo.net ' http://google.com/p8isslord please help the good one www.leafopiss.com yeah what the freak")
-})
